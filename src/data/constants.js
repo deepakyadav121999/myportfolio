6f4af5813ba36ea6460750afba96d135691d3a4a
deepakyadav121999 <dy44444221@gmail.com>
2024-05-27 18:20:07 +0530

    main

---
+++ src/data/constants.js
@@ -0,0 +1,325 @@
+export const Bio = {
+    name: "Deepak Yadav",
+    roles: [
+      "Full Stack Developer",
+      "Front End Developer",
+      "Back End Developer"
+    ],
+    description: " I am a dedicated and passionate Full Stack Web Developer with a solid foundation in Computer Science and Engineering. Graduating with a Bachelor's degree in Computer Science and Engineering in 2022, I embarked on a transformative journey to further enhance my skills. This led me to pursue a rigorous 9-month Full Stack Web Development bootcamp at Newton School, where I honed my expertise in modern web technologies and best practices.",
+    github: "https://github.com/deepakyadav121999",
+    resume: "https://drive.google.com/file/d/1ffZrcMcn8UatXGIaautbbqpV7ADNaETA/view?usp=sharing",
+    linkedin: "https://www.linkedin.com/in/deepak-yadav-094a6a235/",
+    twitter: "https://twitter.com/kjcreddy666",
+    insta: "https://www.instagram.com/deepak_x_121999/",
+    facebook: "https://www.facebook.com/profile.php?id=100051516276567",
+};
+  
+export const skills = [
+    {
+      title: "Programming Languages",
+      skills: [
+        {
+          name: "C++",
+          image: "CPP-logo.png",
+        },
+        {
+          name: "Java",
+          image: "java-logo.svg",
+        }
+      ],
+    },
+    {
+      title: "Frontend",
+      skills: [
+        {
+          name: "HTML",
+          image: "html5-logo.png",
+        },
+        {
+          name: "CSS",
+          image:
+            "CSS3-logo.png",
+        },
+        {
+          name: "Bootstrap",
+          image:
+            "bootstrap-logo.png",
+        },
+        {
+          name: "JavaScript",
+          image:
+            "JavaScript-logo.png",
+        },
+        {
+          name: "React Js",
+          image:
+            "react-logo.png",
+        },
+        {
+          name: "Tailwind",
+          image:
+            "talwind.png",
+        },
+      ],
+    },
+    {
+      title: "Backend",
+      skills: [
+        {
+          name: "Node Js",
+          image: "nodejs-logo.svg",
+        },
+         {
+        name: "Express Js",
+       image: "express.png"
+        
+       },
+       {
+        name: "MongoDB",
+       image: "mongo.png"
+        
+       },
+      ],
+    },
+    {
+      title: "Others",
+      skills: [
+        // {
+        //   name: "Git",
+        //   image:
+        //     "Git-logo.png",
+        // },
+        {
+          name: "GitHub",
+          image:
+            "GitHub-logo.png",
+        },
+        {
+          name: "VS Code",
+          image:
+            "vscode-logo.png",
+        },
+      ],
+    },
+];
+  
+export const experiences = [
+    {
+      id: 0,
+      img: "https://firebasestorage.googleapis.com/v0/b/flexi-coding.appspot.com/o/flipr.jpeg?alt=media&token=1d72532a-45eb-4c1a-a81a-c9bed9fec543",
+      role: "DevOps & Fullstack Engineering Intern",
+      company: "Flipr Inovations Pvt. Ltd.",
+      date: "Aug 2023 - July 2023",
+      desc: "Working on Flipr Platforms, managing DevOps, and streamlining the process with automation.",
+      skills: [
+        "Docker",
+        "Terraform",
+        "AWS",
+        "EC2",
+        "Portainer",
+        "Nginx",
+        "JavaScript",
+        "TypeScript",
+        "Node Js",
+        " Next Js",
+      ],
+      doc: "https://media.licdn.com/dms/image/D4D2DAQFlp60ZqHuaFQ/profile-treasury-image-shrink_1280_1280/0/1691180828512?e=1692381600&v=beta&t=mM5Y_NE5EPlQhez5FAN6NLVSKcO_Ojt_9Gq3mnFGkAQ",
+    },
+    {
+      id: 0,
+      img: "https://firebasestorage.googleapis.com/v0/b/flexi-coding.appspot.com/o/neurobit.jpeg?alt=media&token=1cacbb7b-e5ea-4efb-9b3d-f5a31b9564ad",
+      role: "Frontend Engineer Intern",
+      company: "Neurobit",
+      date: "June 2023 - Present",
+      desc: "Working on the frontend of the web application using ReactJS, Redux, and Material UI.",
+      skills: [
+        "ReactJS",
+        "Redux",
+        "NodeJs",
+        "Material UI",
+        "HTML",
+        "CSS",
+        "JavaScript",
+      ],
+      doc: "https://firebasestorage.googleapis.com/v0/b/buckoid-917cf.appspot.com/o/Screenshot%20from%202023-05-28%2023-20-46.png?alt=media&token=5570f995-e8f4-4f12-bb2f-73bcf4b20730",
+    },
+    {
+      id: 1,
+      img: "https://firebasestorage.googleapis.com/v0/b/flexi-coding.appspot.com/o/flipr.jpeg?alt=media&token=1d72532a-45eb-4c1a-a81a-c9bed9fec543",
+      role: "Fullstack Externship",
+      company: "Flipr",
+      date: "June 2023 - July 2023",
+      desc: "Built an employee management full stack web app used Docker and deployed on AWS ec2. I was the top performer in the program.",
+      skills: [
+        "ReactJS",
+        "Redux",
+        "NodeJs",
+        "Material UI",
+        "HTML",
+        "CSS",
+        "JavaScript",
+        "Docker",
+        "AWS",
+        "MongoDB",
+      ],
+    },
+    {
+      id: 2,
+      img: "https://firebasestorage.googleapis.com/v0/b/flexi-coding.appspot.com/o/girlScript.jpeg?alt=media&token=e656a621-cf3c-4230-bf0f-e74b4cec6035",
+      role: "Open Source Contributor ",
+      company: "GirlScript Summer of Code",
+      date: "May 2023 - Present",
+      desc: "Contributing to different open-source projects and learn from industry experts",
+      doc: "https://media.licdn.com/dms/image/D4D22AQGzSpgFaR4hsQ/feedshare-shrink_800/0/1684079632872?e=1687996800&v=beta&t=i0xWn_k32Oj9SxKfc6rARSbOghXD9Ge6WyEb2p2vvrc",
+    },
+    {
+      id: 3,
+      img: "https://firebasestorage.googleapis.com/v0/b/flexi-coding.appspot.com/o/gdsc.jpeg?alt=media&token=c162329c-efaa-4be8-a173-8d3f4c48ea70",
+      role: "Android Developer",
+      company: "DSC KIIT",
+      date: "Nov2021 - Present",
+      desc: "As an Android developer at the Google Developers Student Club (GDCS), I have had the opportunity to work on exciting projects and collaborate with talented developers who share my passion for technology. Through my involvement with GDCS, I have also had the opportunity to host and participate in numerous events, including hackathons, study jams, and workshops.",
+      skills: [
+        "Leadership",
+        "Mobile Application Development",
+        "Kotlin",
+        "XML",
+        "Figma",
+      ],
+    },
+    {
+      id: 4,
+      img: "https://firebasestorage.googleapis.com/v0/b/flexi-coding.appspot.com/o/Rudraksha.jpeg?alt=media&token=8f83f41e-d0a1-486f-9c7f-dd4cd1d09e8d",
+      role: "Android Developer Intern",
+      company: "Rudraksha Welffare Foundation",
+      date: "June 2021 - Oct 2021",
+      desc: "As an Android Developer Intern at Rudraksha Welfare Foundation from June 2021 to October 2021, I gained valuable hands-on experience in application development. During my tenure, I contributed to the development of the application's back-end cloud functions using Firebase and implemented a front-end with the MVVM structure, which was designed using Figma. I also added a one-to-one video call feature, conceptualized new features, and optimized app performance. This internship helped me enhance my skills in Android development and gave me an opportunity to work in a collaborative team environment.",
+      skills: [
+        "Android",
+        "Java",
+        "Kotlin",
+        "XML",
+        "Node Js",
+        "Cloud Firestore",
+        "Firebase",
+        "Figma",
+      ],
+      doc: "https://firebasestorage.googleapis.com/v0/b/buckoid-917cf.appspot.com/o/WhatsApp%20Image%202023-05-05%20at%2012.07.39%20AM.jpeg?alt=media&token=9f0e1648-568b-422d-bd0b-1f125f722245",
+    },
+];
+  
+export const education = [
+    {
+      id: 0,
+      img: "college1.png",
+      school: "St. Aloysius Institute Of Technology, Jabalpur",
+      date: "2017 - 2022",
+      grade: "6.80 CGPA",
+      desc: "I completed my Bachelor's degree in Computer Science and Engineering at St. Aloysius Institute Of Technology, Jabalpur with a CGPA of 6.8. I have taken courses in Data Structures, Algorithms, Object-Oriented Programming, Database Management Systems, Operating Systems, and Computer Networks, among others.",
+      degree: "Bachelor of Technology - BTech, Computer Science and Engineering",
+    },
+    {
+      id: 1,
+      img: "school.png",
+      school: "Govt Higher Secondary School Maneri, Niwas",
+      date: "2015 - 2017",
+      grade: "5.49 GPA",
+      desc: "I completed my Intermediate at Govt Higher Secondary School Maneri",
+      degree: "Board Of Secondary Education Mp - MPC(Maths, Physics, Chemistry)",
+    },
+    {
+      id: 2,
+      img: "school.png",
+      school: "Govt Higher Secondary School Maneri, Niwas",
+      date: "2013 - 2015",
+      grade: "6.8 GPA",
+      desc: "I completed my class 10 education at Govt Higher Secondary School Maneri.",
+      degree: "Board Of Secondary Education Mp",
+    },
+];
+  
+export const projects = [
+    {
+      id: 9,
+      title: "Meesho Clone",
+      description:
+        "Developed a user-friendly e-commerce website with a range of features, including search functionality, wishlist creation, filtering options, and a seamless shopping cart system",
+      image:
+        "meesho.png",
+      tags: [
+        "HTML5",
+        "CSS3",
+        "JavaScript",
+        "ReactJs",
+        "Redux",
+        "firebase"
+      ],
+      category: "web app",
+      github: "https://github.com/deepakyadav121999/justtry",
+      webapp: "https://project1-omega-one.vercel.app/",
+    },
+
+    {
+      id: 9,
+      title: "Portfolio",
+      description:
+        "Explore my sleek portfolio, built with HTML5, CSS3, JavaScript, and React.js. Discover my skills and projects through seamless design and interactivity, optimized for any device.",
+      image:
+        "port.png",
+      tags: [
+        "HTML5",
+        "CSS3",
+        "JavaScript",
+        "React",
+      ],
+      category: "web app",
+      github: "",
+      webapp: "#",
+    },
+
+    {
+      id: 9,
+      title: "Linkedin Clone",
+      description:
+        "Developed a dynamic web application using React.js that allows users to create, share, and manage personalized lists of favorite links. .",
+      image:
+        "linkedin.png",
+      tags: [
+        "HTML5",
+        "CSS3",
+        "JavaScript",
+        "ReactJs"
+      ],
+      category: "web app",
+      github: "https://github.com/deepakyadav121999/linkedin-clone-final",
+      webapp: "https://clone-linked.vercel.app/",
+    },
+
+    {
+      id: 9,
+      title: "To-do App",
+      description:
+        `"Designed and developed a to-do list web application using React.js, aimed at helping users organize tasks and manage their daily activities efficiently`,
+      image:
+        "todo.png",
+      tags: [
+        "HTML5",
+        "CSS3",
+        "JavaScript",
+        "React"
+      ],
+      category: "web app",
+      github: "https://github.com/deepakyadav121999/to-do-app",
+      webapp: "https://github.com/deepakyadav121999/to-do-app",
+    },
+
+
+];
+  
+export const TimeLineData = [
+    { year: 2017, text: "Started my journey" },
+    { year: 2018, text: "Worked as a freelance developer" },
+    { year: 2019, text: "Founded JavaScript Mastery" },
+    { year: 2020, text: "Shared my projects with the world" },
+    { year: 2021, text: "Started my own platform" },
+];
